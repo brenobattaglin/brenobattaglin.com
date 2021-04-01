b07a62ea66d2ac15be9e67da4d4525a875932a4e
Breno Battaglin <breno.battaglin@icloud.com>
2021-04-01 14:17:22 -0300

    Update favicon

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
 import pic01 from "../images/pic01.jpg";
-import pic02 from "../images/pic02.jpg";
-import pic03 from "../images/pic03.jpg";
-
 class Main extends React.Component {
   render() {
     let close = (
