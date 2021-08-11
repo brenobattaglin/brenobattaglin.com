f116a590ae579477f77a741e580e0d999f9e2058
Breno Battaglin <breno.battaglin@icloud.com>
2021-08-10 22:18:01 -0300

    Fix image size on about page

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,7 +26,7 @@ class Main extends React.Component {
           style={{ display: "none" }}
         >
           <h2 className="major">About</h2>
-          <span className="image xsmall">
+          <span className="image main">
             <img src={pic01} alt="" />
           </span>
           <p>
