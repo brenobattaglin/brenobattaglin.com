6a0181080a3bd776a60e4c17781d20eef45ca752
Breno Battaglin <breno.battaglin@icloud.com>
2021-10-12 17:35:39 -0300

    Create Head and add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import NavBar from './components/navbar'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Breno Battaglin</title>
+        <meta name='description' content='Personal website' />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
