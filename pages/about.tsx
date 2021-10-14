c6cd2d81c2766393e8c493c2c2b59cb22051ce71
Breno Battaglin <breno.battaglin@icloud.com>
2021-10-13 22:18:13 -0300

    Create about page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,28 @@
 import type { NextPage } from 'next'
-import NavBar from './components/navbar'
+import Image from 'next/image'
+
 
 const About: NextPage = () => {
   return (
-    <span>
-      Lorem ipsumasdasd
-    </span>
+    <div className="flex flex-col mx-auto min-h-screen max-w-sm justify-center items-center">
+      <Image
+        src="/images/profile.png"
+        height={144}
+        width={144}
+        alt="Breno's profile photo"
+      />
+      <div className="mt-4">
+        <p>
+          I was born in the state of Espírito Santo, Brazil. When I was a child, I always loved games and tech.
+        </p>
+        <br />
+        <p>
+          Growing up, I decided to do a technical programming course, and later, study computer science.
+          At this time, my carrer begins as web developer.
+          Today I have a bachelor in computer science and work as software engineer.
+        </p>
+      </div>
+    </div>
   )
 }
 
