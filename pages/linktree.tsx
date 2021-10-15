58e52188d35d302492359f786ff58b33c7f7960b
Breno Battaglin <breno.battaglin@icloud.com>
2021-10-15 20:01:51 -0300

    Update linktree page content

--- pages/linktree.tsx
+++ pages/linktree.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
-import NavBar from './components/navbar'
 
 const Linktree: NextPage = () => {
   return (
-    <span>
-      Lorem ipsum
-    </span>
+    <div className="flex flex-col mx-auto min-h-screen max-w-sm justify-center items-center space-y-4">
+      <a className="block" href="https://www.linkedin.com/in/brenobattaglin">Linkedin</a>
+      <a className="block" href="https://github.com/brenobattaglin">Github</a>
+      <a className="block" href="https://instagram.com/brenobattaglin">Instagram</a>
+      <a className="block" href="https://open.spotify.com/user/brenobattaglin">Spotify</a>
+      <a className="block" href="https://twitter.com/brenobattaglin">Twitter</a>
+    </div>
   )
 }
 
