f988df0cc5d8520419a34860f60875afdd2c104b
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-03 20:32:23 -0300

    Add hover effect for all the links

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,15 +7,15 @@ const About: NextPage = () => {
     <div className="flex flex-col mx-auto min-h-screen max-w-sm justify-center items-center">
       <Image
         src="/images/profile.png"
-        height={144}
-        width={144}
+        height={200}
+        width={200}
         alt="Breno's profile photo"
       />
-      <div className="mt-16">
-        <p className="mb-4">
+      <div className="mt-16 p-3" >
+        <p className="mb-4 text-justify">
           I was born in the state of Espírito Santo, Brazil. When I was a child, I always loved games and tech.
         </p>
-        <p>
+        <p className="text-justify">
           Growing up, I decided to do a technical programming course, and later, study computer science.
           At this time, my carrer begins as web developer.
           Today I have a bachelor in computer science and work as software engineer.
--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 const NavBar = () => {
     return (
-        <div className="container mx-auto w-screen space-x-4 text-center">
+        <div className="container mx-auto w-screen space-x-4 text-center pt-3">
             <Link href='/' >
-                <a className="inline align-middle">Home</a>
+                <a className="inline align-middle hover:text-blue-400">Home</a>
             </Link>
             <Link href='/about' >
-                <a className="inline align-middle">About</a>
+                <a className="inline align-middle hover:text-blue-400">About</a>
             </Link>
-            <a href='mailto:breno.battaglin@icloud.com' className="inline align-middle">Contact</a>
+            <a href='mailto:breno.battaglin@icloud.com' className="inline align-middle hover:text-blue-400">Contact</a>
             <Link href='/linktree'>
-                <a className="inline align-middle">Linktree</a>
+                <a className="inline align-middle hover:text-blue-400">Linktree</a>
             </Link>
         </div>
     );
