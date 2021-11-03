0df2c6f2c042f16fe015f0ee61fb799f22f8a1e8
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-03 20:36:25 -0300

    Add Email to Linktree

--- pages/linktree.tsx
+++ pages/linktree.tsx
@@ -3,9 +3,10 @@ import type { NextPage } from 'next'
 const Linktree: NextPage = () => {
   return (
     <div className="flex flex-col mx-auto min-h-screen max-w-sm justify-center items-center space-y-4">
-      <a className="block hover:text-blue-400" href="https://www.linkedin.com/in/brenobattaglin">Linkedin</a>
+      <a className="block hover:text-blue-400" href="mailto:breno.battaglin@icloud.com">Email</a>
       <a className="block hover:text-blue-400" href="https://github.com/brenobattaglin">Github</a>
       <a className="block hover:text-blue-400" href="https://instagram.com/brenobattaglin">Instagram</a>
+      <a className="block hover:text-blue-400" href="https://www.linkedin.com/in/brenobattaglin">Linkedin</a>
       <a className="block hover:text-blue-400" href="https://open.spotify.com/user/brenobattaglin">Spotify</a>
       <a className="block hover:text-blue-400" href="https://twitter.com/brenobattaglin">Twitter</a>
     </div>
