f36dff5a57073d008f6b8b093f49d674053446fc
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-08 19:33:51 -0300

    Refactor and create linktree animations

--- pages/linktree.tsx
+++ pages/linktree.tsx
@@ -1,19 +1,33 @@
 import { motion } from 'framer-motion'
 import type { NextPage } from 'next'
-import contentVariants from './components/animationVariants/contentVariants'
+import { redirect } from 'next/dist/server/api-utils';
+import contentVariants from './components/animation/contentVariants'
+import linkVariants from './components/animation/linkVariants';
+
+const links = [
+  { url: 'mailto:breno.battaglin@icloud.com', description: 'Email', mustOpenTab: false },
+  { url: 'https://github.com/brenobattaglin', description: 'Github', mustOpenTab: true },
+  { url: 'https://instagram.com/brenobattaglin', description: 'Instagram', mustOpenTab: true },
+  { url: 'https://www.linkedin.com/in/brenobattaglin', description: 'LinkedIn', mustOpenTab: true },
+  { url: 'https://open.spotify.com/user/brenobattaglin', description: 'Spotify', mustOpenTab: true },
+  { url: 'https://twitter.com/brenobattaglin', description: 'Twitter', mustOpenTab: true },
+];
 
 const Linktree: NextPage = () => {
   return (
-    <motion.div initial="exit" animate="enter" exit="exit" variants={contentVariants} >
+    <motion.div variants={contentVariants} initial="hidden" animate="visible" exit="exit">
       <div className="flex flex-col mx-auto min-h-screen max-w-sm justify-center items-center space-y-4">
-        <a className="block text-nord6 hover:text-blue-400" href="mailto:breno.battaglin@icloud.com">Email</a>
-        <a className="block text-nord6 hover:text-blue-400" target="_blank" href="https://github.com/brenobattaglin">Github</a>
-        <a className="block text-nord6 hover:text-blue-400" target="_blank" href="https://instagram.com/brenobattaglin">Instagram</a>
-        <a className="block text-nord6 hover:text-blue-400" target="_blank" href="https://www.linkedin.com/in/brenobattaglin">Linkedin</a>
-        <a className="block text-nord6 hover:text-blue-400" target="_blank" href="https://open.spotify.com/user/brenobattaglin">Spotify</a>
-        <a className="block text-nord6 hover:text-blue-400" target="_blank" href="https://twitter.com/brenobattaglin">Twitter</a>
+        {
+          links.map(
+            (link) => (
+              <motion.a variants={linkVariants} whileHover="hover" whileTap="tap" >
+                <a className="block text-nord6 hover:text-nord8" target="_blank" href={link.url}>{link.description}</a>
+              </motion.a>
+            )
+          )
+        }
       </div>
-    </motion.div>
+    </motion.div >
   )
 }
 
