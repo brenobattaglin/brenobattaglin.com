15a73bd08ed4b111c76d5edf5237b47df2fcd7d5
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-08 21:00:33 -0300

    Create subtitle logic on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from 'next'
 import Content from './components/content';
 
+const subtitles = [
+  { id: 1, text: 'Computer Scientist' },
+  { id: 2, text: 'Software Engineer' },
+  { id: 3, text: 'Minimalist' },
+];
+
 const Home: NextPage = () => {
   return (
     <Content>
       <p className="text-4xl mb-4 text-nord6">Breno Battaglin</p>
-      <p className="text-base text-nord6">Computer Scientist</p>
-      <p className="text-base text-nord6">Software Engineer</p>
-      <p className="text-base text-nord6">Minimalist</p>
+      {
+        subtitles.map(
+          (subtitle) => (
+            <p key={subtitle.id} className="text-base text-nord6">{subtitle.text}</p>
+          )
+        )
+      }
     </Content>
   )
 }
