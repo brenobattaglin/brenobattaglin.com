7ab7478a08a9e9b207075b7e2168bd5882b1b83a
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-08 21:22:46 -0300

    Implement logic to open new tab on linktree page

--- pages/linktree.tsx
+++ pages/linktree.tsx
@@ -4,12 +4,12 @@ import linktreeVariants from './components/animation/linktreeVariants';
 import Content from './components/content';
 
 const links = [
-  { id: 1, url: 'mailto:breno.battaglin@icloud.com', description: 'Email', mustOpenTab: false },
-  { id: 2, url: 'https://github.com/brenobattaglin', description: 'Github', mustOpenTab: true },
-  { id: 3, url: 'https://instagram.com/brenobattaglin', description: 'Instagram', mustOpenTab: true },
-  { id: 4, url: 'https://www.linkedin.com/in/brenobattaglin', description: 'LinkedIn', mustOpenTab: true },
-  { id: 5, url: 'https://open.spotify.com/user/brenobattaglin', description: 'Spotify', mustOpenTab: true },
-  { id: 6, url: 'https://twitter.com/brenobattaglin', description: 'Twitter', mustOpenTab: true },
+  { id: 1, url: 'mailto:breno.battaglin@icloud.com', description: 'Email', keepActiveTab: true },
+  { id: 2, url: 'https://github.com/brenobattaglin', description: 'Github' },
+  { id: 3, url: 'https://instagram.com/brenobattaglin', description: 'Instagram' },
+  { id: 4, url: 'https://www.linkedin.com/in/brenobattaglin', description: 'LinkedIn' },
+  { id: 5, url: 'https://open.spotify.com/user/brenobattaglin', description: 'Spotify' },
+  { id: 6, url: 'https://twitter.com/brenobattaglin', description: 'Twitter' },
 ];
 
 const Linktree: NextPage = () => {
@@ -17,11 +17,19 @@ const Linktree: NextPage = () => {
     <Content>
       {
         links.map(
-          (link) => (
-            <motion.a key={link.id} variants={linktreeVariants} whileHover="hover" whileTap="tap"
-              className="block text-nord6 hover:text-nord8 my-2" target="_blank" href={link.url}>{link.description}
-            </motion.a>
-          )
+          (link) => {
+            return (
+              <motion.a key={link.id}
+                variants={linktreeVariants}
+                whileHover="hover"
+                whileTap="tap"
+                className="block text-nord6 hover:text-nord8 my-2"
+                {...!link.keepActiveTab && { target: '_blank' }}
+                href={link.url}>
+                {link.description}
+              </motion.a>
+            );
+          }
         )
       }
     </Content>
