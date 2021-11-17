36317dc571494c46d6a33a6768711f63a9264d33
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-17 20:10:24 -0300

    WIP background animation

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,9 +1,28 @@
 import NavBar from "./navbar"
 import Head from "next/head"
-import { AnimatePresence, motion } from "framer-motion"
-import Content from "./content"
+import { BufferGeometry, Line, LineBasicMaterial, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, TorusGeometry, Vector3, WebGLRenderer } from 'three';
+import { useEffect } from "react";
 
 export default function Layout({ children }: { children: any }) {
+    useEffect(() => {
+        const renderer = new WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+        camera.position.set(0, 0, 100);
+        camera.lookAt(0, 0, 0);
+
+        const geometry = new TorusGeometry(10, 0.1, 15, 3, 6.3);
+        const material = new MeshBasicMaterial({ color: 0xffff00 });
+        const torus = new Mesh(geometry, material);
+
+        const scene = new Scene();
+        scene.add(torus);
+
+        renderer.render(scene, camera);
+    }, [])
+
     return (
         <>
             <Head>
