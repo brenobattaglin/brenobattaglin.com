badbd4b1a642e8fc5eb5f1ce80a30eefcd0c0f57
Breno Battaglin <breno.battaglin@icloud.com>
2023-01-04 21:10:19 -0300

    Move components folder
    - Move folder to root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/global.css";
-import Layout from "./components/layout";
+import Layout from "../components/layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
