77888c83477c505c0e1ca1a60faa675457895e0d
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2025-05-16 11:19:07 -0300

    style: update background animation

--- components/background.tsx
+++ components/background.tsx
@@ -1,34 +1,39 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { TorusKnot } from '@react-three/drei'
-
-const BackgroundAnimation = () => {
-    const mesh = useRef<THREE.Mesh>(null!)
-
-    useFrame(() => {
-        mesh.current.rotateY(0.005)
-    });
-
-    return (
-        <TorusKnot
-            args={[6, 2.3, 11, 3, 5, 11]}
-            ref={mesh}
-        >
-            <meshBasicMaterial
-                color="#2e3440"
-                wireframe={true}
-            />
-        </TorusKnot>
-    );
-}
+import { RoundedBox } from '@react-three/drei'
 
 export default function Background() {
     return (
         <Canvas
-            camera={{ position: [0, 0, 35] }}
+            camera={{ position: [0, 0, 1] }}
             style={{ zIndex: -1, position: 'absolute' }}
         >
             <BackgroundAnimation />
         </Canvas >
     )
 }
+
+const BackgroundAnimation = () => {
+    const mesh = useRef<THREE.Mesh>(null!)
+
+    useFrame(() => {
+        mesh.current.rotateY(0.001)
+        mesh.current.rotateX(0.001)
+        mesh.current.rotateZ(0.001)
+    });
+
+    return (
+        <RoundedBox
+           args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+            radius={3} // Radius of the rounded corners. Default is 0.05
+            smoothness={5} // The number of curve segments. Default is 4
+            ref={mesh}
+        >
+            <meshBasicMaterial
+                color="#2e3440"
+                wireframe={true}
+            />
+        </RoundedBox>
+    );
+}
+
