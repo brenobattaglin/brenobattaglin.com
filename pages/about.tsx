ef0197fea4f7627295d02df363e0361d6dc04332
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2025-05-16 16:24:25 -0300

    style: update about page
    - update image size
    
    - update about description

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,17 +3,16 @@ import Image from 'next/image';
 import Content from '../components/core/content';
 import BackLink from '../components/core/back-link';
 
+
 const paragraphs = [
   {
     id: 1,
-    text: 'I was born in the state of Espírito Santo, Brazil. When I was a child, I always loved games and tech.',
+    text: 'Hi, I\'m Breno.'
   },
   {
     id: 2,
-    text:
-      'Growing up, I decided to do a programming technical course and later, study computer science. ' +
-      'At this time, my carrer begins as web developer. ' +
-      'Today I have a bachelor in computer science and work as software engineer.',
+    text: 'I\'m a Computer Scientist and Software Engineer, passionate about creating elegant and efficient solutions. ' +
+    'I\'m driven by challenges, commited to excellence, and dedicated to continuous learning.'
   },
 ];
 
@@ -22,8 +21,8 @@ const About: NextPage = () => {
     <Content>
       <Image
         src="/images/profile.jpg"
-        height={200}
-        width={200}
+        height={150}
+        width={150}
         alt="Breno's profile photo"
         className="rounded-full"
       />
