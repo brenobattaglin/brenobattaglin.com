81f42b6d6bdbf5cae5506b464312d1c9fe40476e
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2025-11-30 14:47:13 -0300

    feat: create intro section

---
+++ components/content/intro.tsx
@@ -0,0 +1,17 @@
+import { EncryptedText } from '@/components/ui/encrypted-text';
+
+export default function IntroSection() {
+  const title = 'Breno Battaglin';
+  const subtitle = 'Software Engineer';
+
+  return (
+        <section className="snap-start h-screen flex items-center justify-center">
+          <div className="text-center">
+            <p className="text-4xl mb-2 text-white">
+              <EncryptedText text={title} />
+            </p>
+            <p className="text-sm text-gray-400">{subtitle}</p>
+          </div>
+        </section>
+  );
+}
