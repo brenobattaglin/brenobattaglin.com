6ddf3f090b580f267a9d6521ebc76244cade857d
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2025-11-30 22:16:00 -0300

    feat: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,48 @@
-import IntroSection from '@/components/sections/intro';
-import AboutSection from '@/components/sections/about';
-import LinksSection from '@/components/sections/links';
-import ProjectsSection from '@/components/sections/projects';
+import IntroSection from '@/components/content/intro';
+import ContactSection from '@/components/content/contact';
+import ProjectsSection from '@/components/content/projects';
 import { FloatingDock } from '@/components/ui/floating-dock';
-import { IconHome, IconUser, IconLink, IconBriefcase, IconNavigation } from '@tabler/icons-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import UserIcon from '@/components/icons/user';
+import BriefcaseIcon from '@/components/icons/briefcase';
+import MessageIcon from '@/components/icons/message';
 
 export default function Home() {
   const dockItems = [
     {
-      title: 'Top',
-      icon: <IconNavigation className="h-full w-full" />,
+      title: 'Go to Top',
+      icon: <UserIcon />,
       href: '#intro',
     },
     {
-      title: 'About',
-      icon: <IconUser className="h-full w-full" />,
-      href: '#about',
-    },
-    {
-      title: 'Links',
-      icon: <IconLink className="h-full w-full" />,
-      href: '#links',
-    },
-    {
       title: 'Projects',
-      icon: <IconBriefcase className="h-full w-full" />,
+      icon: <BriefcaseIcon />,
       href: '#projects',
     },
+    {
+      title: 'Contact Me',
+      icon: <MessageIcon />,
+      href: '#contact',
+    },
+
   ];
 
   return (
-    <div className="h-screen overflow-y-scroll">
-      <div id="intro">
+    <ScrollArea className="h-screen snap-y snap-mandatory">
+      <div id="intro" className="snap-start snap-always">
         <IntroSection />
       </div>
-      <div id="about">
-        <AboutSection />
-      </div>
-      <div id="links">
-        <LinksSection />
-      </div>
-      <div id="projects">
+      <div id="projects" className="snap-start snap-always">
         <ProjectsSection />
       </div>
-      
+      <div id="contact" className="snap-start snap-always">
+        <ContactSection />
+      </div>
+   
+
       <div className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50">
         <FloatingDock items={dockItems} />
       </div>
-    </div>
+    </ScrollArea>
   );
 }
